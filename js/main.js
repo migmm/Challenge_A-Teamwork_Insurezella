ea18033d9622e36a4c64e82a5c396b74457d7167
Miguel <micmigm@gmail.com>
2024-08-27 18:06:51 -0300

    Added function to close nav menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+
 /* 
 
 ¯\_(ツ)_/¯
 
 */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Closes the navigation menu when an item is clicked
+
+    const menuLinks = document.querySelectorAll('#navbar-menu ul a');
+    const menuToggle = document.getElementById('menuToggle');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            menuToggle.checked = false;
+        });
+    });
+});
